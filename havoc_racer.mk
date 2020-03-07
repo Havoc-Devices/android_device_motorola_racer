4bd7ade7b4f53a88da194bf72108cb9e8e181e53
Andrew Hexen <SyberHexen@gmail.com>
2020-03-07 11:21:47 -0800

    racer: havoc-ify
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>
    Change-Id: Ic9028e4329cf0e3a7493a7b8e7e8723bff44766d

--- havoc_racer.mk
+++ havoc_racer.mk
@@ -21,11 +21,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from racer device
 $(call inherit-product, device/motorola/racer/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common havoc stuff.
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_racer
+PRODUCT_NAME := havoc_racer
 PRODUCT_DEVICE := racer
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := Motorola Edge
